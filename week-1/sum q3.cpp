1a120f8d24063f4ee52f3bacad140216918739fb
shibana1412008-maker <shibana14.1.2008@gmail.com>
2025-10-23 11:39:12 +0530

    Add files via upload

---
+++ week-1/sum q3.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+	int i,n;
+	scanf("%d",&n);
+	int arr[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	int pos=0,neg=0,zero=0;
+	for(i=0;i<n;i++)
+	{
+		if(arr[i]>0){
+			pos++;
+		}
+		else if(arr[i]<0){
+			neg++;
+		}
+		else{
+			zero++;
+		}
+		}
+		printf("positive:%d\n",pos);
+		printf("negative:%d\n",neg);
+		printf("zero:%d",zero);
+		return 0;
+	}
+		
+	
+			
+		
+	
+
